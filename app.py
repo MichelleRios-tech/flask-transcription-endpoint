9eaa40fdd3f80ba4587635785bdbbf72d3a61608
Irving Michelle Rios Castellanos <gamma-1994@hotmail.com>
2024-07-03 11:53:23 -0600

    Added fallback to cpu if no cuda device is found

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import torch
 
 from flask import Flask, request, jsonify
 from faster_whisper import WhisperModel
@@ -6,7 +7,11 @@ from faster_whisper import WhisperModel
 app = Flask(__name__)
 model_size = "large-v3"
 
-model = WhisperModel(model_size, device="cuda", compute_type="float16")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+compute_type = "int8" if device == "cpu" else "float16"
+
+model = WhisperModel(model_size, device=device, compute_type=compute_type)
 
 
 @app.route("/transcribe", methods=["POST"])
@@ -20,13 +25,16 @@ def transcribe_audio():
     if audio_file:
         audio_file.save(filename)
 
-        segments, info = model.transcribe(filename, beam_size=5)
+        segments, _ = model.transcribe(filename, beam_size=5)
         os.remove(filename)
         print(f"File {filename} removed successfully.")
         transcription = " ".join(segment.text for segment in segments)
 
         return jsonify({"transcription": transcription})
 
+@app.route("/", methods=["GET"])
+def hello():
+    return "<h1>Hello, World!</h1>"
 
 if __name__ == '__main__':
     app.run()
