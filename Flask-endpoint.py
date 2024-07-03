f57bdc2985dfb457832e4bbd2eacf4d9f9ae3048
Irving Michelle Rios Castellanos <gamma-1994@hotmail.com>
2024-07-03 03:24:39 -0600

    Implement removal of the file after processing

--- Flask-endpoint.py
+++ Flask-endpoint.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 from faster_whisper import WhisperModel
 
@@ -9,7 +11,6 @@ model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
 @app.route("/transcribe", methods=["POST"])
 def transcribe_audio():
-    print(request.files)
     if "audio" not in request.files:
         return jsonify({"error": "No audio file provided"}), 400
 
@@ -20,6 +21,8 @@ def transcribe_audio():
         audio_file.save(filename)
 
         segments, info = model.transcribe(filename, beam_size=5)
+        os.remove(filename)
+        print(f"File {filename} removed successfully.")
         transcription = " ".join(segment.text for segment in segments)
 
         return jsonify({"transcription": transcription})
