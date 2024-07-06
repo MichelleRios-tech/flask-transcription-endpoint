c85707f94c552d456b322a0d5077b5f2a29c4ca4
Irving Michelle Rios Castellanos <gamma-1994@hotmail.com>
2024-07-06 10:07:15 -0600

    separate the transcription logic on its own service

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import os
-
 from flask import Flask, request, jsonify
 from faster_whisper import WhisperModel
 from pytube import YouTube
 
+from services import transcription_service
+
 app = Flask(__name__)
-model_size = "large-v3"
-
-model = WhisperModel(model_size, device="cuda", compute_type="float16")
-
 
 @app.route("/transcribe", methods=["POST"])
 def transcribe_audio():
@@ -22,31 +18,16 @@ def transcribe_audio():
         return jsonify({"error": "No audio file provided"}), 400
 
     if audio_file:
-        transcription = transcribe_from_file(audio_file)
+        transcription = transcription_service.transcribe_from_file(audio_file)
     else:
-        transcription = transcribe_from_youtube(audio_link)
+        transcription = transcription_service.transcribe_from_youtube(audio_link)
 
     return jsonify({"transcription": transcription})
 
-def transcribe_from_file(audio_file):
-    print("Transcribing audio file", audio_file.filename)
-    filename = audio_file.filename + ".mp3"
-    audio_file.save(filename)
-
-    return generate_transcription(filename)
-
-def generate_transcription(filename):
-    segments, _ = model.transcribe(filename, beam_size=5)
-    os.remove(filename)
-    print(f"File {filename} removed successfully.")
-    return " ".join(segment.text for segment in segments)
-
-def transcribe_from_youtube(link):
-    print("Downloading audio from YouTube link", link)
-    yt = YouTube(link)
-    audio_file = yt.streams.filter(only_audio=True).first().download()
-
-    return generate_transcription(audio_file)
+# sanity check endpoint to make sure the server is running
+@app.route("/ping", methods=["GET"])
+def ping():
+    return jsonify({"message": "pong!"})
 
 if __name__ == '__main__':
     app.run()
---
+++ services/transcription_service.py
@@ -0,0 +1,28 @@
+import os
+
+from faster_whisper import WhisperModel
+from pytube import YouTube
+
+model_size = "large-v3"
+
+model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+
+def transcribe_from_file(audio_file):
+    print("Transcribing audio file", audio_file.filename)
+    filename = audio_file.filename + ".mp3"
+    audio_file.save(filename)
+    return generate_transcription(filename)
+
+def generate_transcription(filename):
+    segments, _ = model.transcribe(filename, beam_size=5)
+    os.remove(filename)
+    print(f"File {filename} removed successfully.")
+    return " ".join(segment.text for segment in segments)
+
+def transcribe_from_youtube(link):
+    print("Downloading audio from YouTube link", link)
+    yt = YouTube(link)
+    audio_file = yt.streams.filter(only_audio=True).first().download()
+
+    return generate_transcription(audio_file)
